3a285ac94df40cd235b3dfa6aa8b79658d902b5a
BAEKJungHo <designjava@naver.com>
2023-08-20 13:31:57 +0900

    Modify

--- js/create-link.js
+++ js/create-link.js
@@ -123,7 +123,7 @@
     for (let i = 0; i < links.length; i++) {
         const link = links[i];
         const url = link.getAttribute('href');
-        if (/^(https?:\/\/)?johngrib\.github\.io/.test(url) || /^[\/#]/.test(url)) {
+        if (/^(https?:\/\/)?baekjungho\.github\.io/.test(url) || /^[\/#]/.test(url)) {
             // inner link
         } else {
             // external link
